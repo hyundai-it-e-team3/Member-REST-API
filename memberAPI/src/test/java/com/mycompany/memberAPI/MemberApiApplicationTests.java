eb005ce73fb06e64610c87da31f7df523af2f125
YENA <dpsk331@naver.com>
2021-11-23 15:35:10 +0900

    Project first commit

---
+++ memberAPI/src/test/java/com/mycompany/memberAPI/MemberApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mycompany.memberAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MemberApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
