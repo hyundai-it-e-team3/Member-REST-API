6b9b8c408f7c7a3afb630cd680de8745e02a7947
YENA <dpsk331@naver.com>
2021-11-25 01:18:32 +0900

    HYUN-89 <RestAPI 회원주소 조회/추가/수정/삭제>

---
+++ memberAPI/src/main/java/com/mycompany/memberAPI/controller/AddressController.java
@@ -0,0 +1,57 @@
+package com.mycompany.memberAPI.controller;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.mycompany.memberAPI.dto.Address;
+import com.mycompany.memberAPI.service.AddressService;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+@RequestMapping("/address")
+public class AddressController {
+	
+	@Resource
+	private AddressService addressService;
+	
+	//주소 조회
+	@GetMapping
+	public List<Address> getAddress(@RequestBody String memberId) {
+		log.info("주소 목록 조회");
+		List<Address> address = addressService.getAddress(memberId);
+		return address;
+	}
+	
+	//주소 추가
+	@PostMapping
+	public void insertAddress(@RequestBody Address address) {
+		log.info("주소 추가");
+		addressService.insertAddress(address);
+	}
+	
+	//주소 수정
+	@PatchMapping
+	public void updateAddress(@RequestBody Address address) {
+		log.info("주소 수정");
+		addressService.updateAddress(address);
+	}
+	
+	
+	//주소 삭제
+	@DeleteMapping
+	public void deleteAddress(@RequestBody Address address) {
+		log.info("주소 삭제");
+		addressService.deleteAddress(address);
+	}
+}
